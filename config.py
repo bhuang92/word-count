8c25cb3fc14ba17df5e2ef4768f6de33fc085fa6
bhuang92 <basilhuang2014@u.northwestern.edu>
2016-11-22 10:24:45 -0600

    migration stuff for db

--- config.py
+++ config.py
@@ -6,6 +6,7 @@ class Config(object):
     TESTING = False
     CSRF_ENABLED = True
     SECRET_KEY = 'needs-to-change'
+    SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
 
 class ProductionConfig(Config):
     Debug = False
